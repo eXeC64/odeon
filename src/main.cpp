c7790969664d38af1b931955c945c7cdedf714dc
Harry Jeffery <harry@exec64.co.uk>
2016-08-12 11:43:59 +0100

    Remove cruft

--- src/main.cpp
+++ src/main.cpp
@@ -20,36 +20,9 @@ int main(int argc, char **argv)
   db.Fetch();
   std::cout << " done." << std::endl;
 
-  /* UI ui(db); */
   CLI ui(db);
 
   ui.Run();
-
-
-  /* for(auto cinema_id : db.GetCinemas()) */
-  /* { */
-  /*   auto cinema = db.GetCinema(cinema_id); */
-  /*   for(auto perf_id : db.GetCinemaPerformances(cinema_id)) */
-  /*   { */
-  /*     auto perf = db.GetPerformance(perf_id); */
-  /*     auto film = db.GetFilm(perf.film); */
-  /*     std::cout << cinema.name << " is showing " << film.title << " on "; */
-  /*     std::cout << perf.day << "/" << perf.month << "/" << perf.year; */
-  /*     std::cout << " at "; */
-  /*     std::cout << std::setw(2) << std::setfill('0') << perf.hour; */
-  /*     std::cout << ":"; */
-  /*     std::cout << std::setw(2) << std::setfill('0') << perf.minute; */
-  /*     if(perf.is3D || perf.isIMAX) */
-  /*     { */
-  /*       std::cout << " in"; */
-  /*       if(perf.isIMAX) */
-  /*         std::cout << " IMAX"; */
-  /*       if(perf.is3D) */
-  /*         std::cout << " 3D"; */
-  /*     } */
-  /*     std::cout << std::endl; */
-  /*   } */
-  /* } */
 
   return 0;
 }
